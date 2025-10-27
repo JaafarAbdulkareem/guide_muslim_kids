56f7c5c27751a2c5aadb9d42138144f37683e1c6
King <jafaar.abdulkareem@gmail.com>
2025-10-27 19:55:45 +0530

    Add Islamic prayer/supplication images and update AppImage class
    Added 13 new image assets related to Islamic rituals:
    - afterPrayer.png
    - fastingArgumentResponse.png
    - iftarAtHome.png
    - iftarSupplication.png
    - openingSupplication.png
    - prayer.png
    - qunootWitr.png
    - risingFromRuku.png
    - ruku.png
    - seeingCrescent.png
    - sujood.png
    - tashahhud.png
    - visitingGraves.png
    
    Updated `app_images.dart` to include corresponding constants for all new images in the AppImage utility class.

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -6,6 +6,8 @@ class AppImage {
 
   static const String imagesAfterEating = "assets/images/afterEating.png";
 
+  static const String imagesAfterPrayer = "assets/images/afterPrayer.png";
+
   static const String imagesAfterRainfall = "assets/images/afterRainfall.png";
 
   static const String imagesAfterWudu = "assets/images/afterWudu.png";
@@ -26,7 +28,13 @@ class AppImage {
 
   static const String imagesEnteringMosque = "assets/images/enteringMosque.png";
 
+  static const String imagesFastingArgumentResponse = "assets/images/fastingArgumentResponse.png";
+
   static const String imagesHearingThunder = "assets/images/hearingThunder.png";
+
+  static const String imagesIftarAtHome = "assets/images/iftarAtHome.png";
+
+  static const String imagesIftarSupplication = "assets/images/iftarSupplication.png";
 
   static const String imagesKafaratAlMajlis = "assets/images/kafaratAlMajlis.png";
 
@@ -36,15 +44,33 @@ class AppImage {
 
   static const String imagesLeavingMosque = "assets/images/leavingMosque.png";
 
+  static const String imagesOpeningSupplication = "assets/images/openingSupplication.png";
+
   static const String imagesPaper = "assets/images/paper.png";
+
+  static const String imagesPrayer = "assets/images/prayer.png";
+
+  static const String imagesQunootWitr = "assets/images/qunootWitr.png";
 
   static const String imagesRainfall = "assets/images/rainfall.png";
 
   static const String imagesRidingCar = "assets/images/ridingCar.png";
 
+  static const String imagesRisingFromRuku = "assets/images/risingFromRuku.png";
+
+  static const String imagesRuku = "assets/images/ruku.png";
+
+  static const String imagesSeeingCrescent = "assets/images/seeingCrescent.png";
+
   static const String imagesSleeping = "assets/images/sleeping.png";
 
   static const String imagesSneezing = "assets/images/sneezing.png";
+
+  static const String imagesSujood = "assets/images/sujood.png";
+
+  static const String imagesTashahhud = "assets/images/tashahhud.png";
+
+  static const String imagesVisitingGraves = "assets/images/visitingGraves.png";
 
   static const String imagesVisitingSick = "assets/images/visitingSick.png";
 
