e48a6fa1f2b6b2c636accc2ca52c7ba446fe9fde
King <jafaar.abdulkareem@gmail.com>
2025-10-27 08:47:45 +0530

    Add new image assets and utility class for daily Islamic practices
    Added 23 new image files related to common daily Islamic rituals and scenarios, including adhan, wudu, eating, entering/exiting homes/mosques, and more. Created `app_images.dart` utility class to centralize image path constants for easy reference throughout the application.
    
    This change provides a structured way to access these assets while maintaining consistency in file naming and organization within the assets directory.

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: prefer_single_quotes
+class AppImage {
+  AppImage._();
+  
+  static const String imagesAdhan = "assets/images/adhan.png";
+
+  static const String imagesAfterEating = "assets/images/afterEating.png";
+
+  static const String imagesAfterWudu = "assets/images/afterWudu.png";
+
+  static const String imagesAthkarDaily = "assets/images/athkarDaily.png";
+
+  static const String imagesBeforeEating = "assets/images/beforeEating.png";
+
+  static const String imagesBeforeWudu = "assets/images/beforeWudu.png";
+
+  static const String imagesEnteringBathroom = "assets/images/enteringBathroom.png";
+
+  static const String imagesEnteringHome = "assets/images/enteringHome.png";
+
+  static const String imagesEnteringMarket = "assets/images/enteringMarket.png";
+
+  static const String imagesEnteringMosque = "assets/images/enteringMosque.png";
+
+  static const String imagesHearingThunder = "assets/images/hearingThunder.png";
+
+  static const String imagesKafaratAlMajlis = "assets/images/kafaratAlMajlis.png";
+
+  static const String imagesLeavingBathroom = "assets/images/leavingBathroom.png";
+
+  static const String imagesLeavingHome = "assets/images/leavingHome.png";
+
+  static const String imagesLeavingMosque = "assets/images/leavingMosque.png";
+
+  static const String imagesPaper = "assets/images/paper.png";
+
+  static const String imagesRainfall = "assets/images/rainfall.png";
+
+  static const String imagesRidingCar = "assets/images/ridingCar.png";
+
+  static const String imagesSleeping = "assets/images/sleeping.png";
+
+  static const String imagesSneezing = "assets/images/sneezing.png";
+
+  static const String imagesVisitingSick = "assets/images/visitingSick.png";
+
+  static const String imagesWakingUpFromSleep = "assets/images/wakingUpFromSleep.png";
+
+  static const String imagesWearingClothes = "assets/images/wearingClothes.png";
+
+  static const String imagesWearingNewClothes = "assets/images/wearingNewClothes.png";
+}
+
