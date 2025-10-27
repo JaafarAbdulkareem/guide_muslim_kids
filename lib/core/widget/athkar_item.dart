2b3a8296eb0f6a8d4f5c85b908ba02cbcd7453b3
King <jafaar.abdulkareem@gmail.com>
2025-10-27 12:10:14 +0530

    Create AthkarItem widget and replace usage in BodyHomeView
    Replace AthkarItemDetail with new AthkarItem placeholder widget in home view.
    Remove unused imports for Daily data and AthkarItemDetail. The new widget currently
    uses a placeholder and will likely be extended later to display athkar items.
    
    This change simplifies the home view by removing dependency on getDataDaily()
    index access while maintaining the same UI structure.

---
+++ lib/core/widget/athkar_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AthkarItem extends StatelessWidget {
+  const AthkarItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/feature/home/ui/widget/body_home_view.dart
+++ lib/feature/home/ui/widget/body_home_view.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:guide_muslim_kids/core/data/daily.dart';
-import 'package:guide_muslim_kids/core/widget/athkar_item_detail.dart';
+import 'package:guide_muslim_kids/core/widget/athkar_item.dart';
 
 class BodyHomeView extends StatelessWidget {
   const BodyHomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AthkarItemDetail(data: getDataDaily(context)[0]);
+    return AthkarItem();
   }
 }
