be9d47b57b8f615ba37b0b00b33620770cc2a291
King <jafaar.abdulkareem@gmail.com>
2025-10-27 19:57:19 +0530

    Remove imageBackground from AthkarEntity
    Removed the imageBackground field and its required constructor parameter from the AthkarEntity class. The imageCharacter field remains unchanged.

--- lib/core/entities/athkar_entity.dart
+++ lib/core/entities/athkar_entity.dart
@@ -2,12 +2,10 @@ class AthkarEntity {
   final String title;
   final String content;
   final String imageCharacter;
-  final String imageBackground;
 
   const AthkarEntity({
     required this.title,
     required this.content,
     required this.imageCharacter,
-    required this.imageBackground,
   });
 }
