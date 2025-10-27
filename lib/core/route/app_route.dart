4ab27ea5b723fd0569898b88bc3e16fc4373496c
King <jafaar.abdulkareem@gmail.com>
2025-10-27 13:18:02 +0530

    Add daily athkar details route and navigation support
    Update AppRoute to handle RouteName.dailyDetails with argument passing
    Enhance RouteX extension methods to support navigation arguments
    Add RouteName.dailyDetails constant for new route identification
    Update HomeView route to use consistent formatting
    
    Changes include:
    - Added AthkarDetailView integration in route handler
    - Implemented argument passing for detailed view navigation
    - Extended navigation extensions with parameter support
    - Added new route constant for daily athkar details
    
    Fixes route handling for athkar entity navigation between screens.

--- lib/core/route/app_route.dart
+++ lib/core/route/app_route.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:guide_muslim_kids/core/entities/athkar_entity.dart';
 import 'package:guide_muslim_kids/core/route/route_name.dart';
+import 'package:guide_muslim_kids/core/widget/athkar_detail_view.dart';
 import 'package:guide_muslim_kids/feature/home/ui/home_view.dart';
 
 class AppRoute {
@@ -8,7 +10,14 @@ class AppRoute {
       // case RouteName.splash:
       // return MaterialPageRoute(builder: (_) => const SplashView());
       case RouteName.home:
-      return MaterialPageRoute(builder: (_) => const HomeView());
+        return MaterialPageRoute(builder: (_) => const HomeView());
+      case RouteName.dailyDetails:
+        final args = settings.arguments;
+        return MaterialPageRoute(
+          builder: (_) =>
+              AthkarDetailView(data: args is AthkarEntity ? args : null),
+        );
+      
       default:
         return MaterialPageRoute(builder: (_) => const Center());
     }
--- lib/core/route/extension.dart
+++ lib/core/route/extension.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 extension RouteX on String {
-  Future pushReplacementNamed(context) =>
-      Navigator.of(context).pushReplacementNamed(this);
-  Future pushNamed(context) => Navigator.of(context).pushNamed(this);
+  Future pushNamed(BuildContext context, {Object? arguments}) =>
+      Navigator.of(context).pushNamed(this, arguments: arguments);
+
+  Future pushReplacementNamed(BuildContext context, {Object? arguments}) =>
+      Navigator.of(context).pushReplacementNamed(this, arguments: arguments);
 }
 
 extension NavigatorX on BuildContext {
-  void pop() => Navigator.of(this).pop();
+  void pop([Object? result]) => Navigator.of(this).pop(result);
 }
