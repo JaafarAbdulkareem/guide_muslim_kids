38eaeeb72c4c04875196276f61aa85af5a9e2ba6
King <jafaar.abdulkareem@gmail.com>
2025-11-08 00:23:31 +0530

    Add audioKey to AthkarEntity and populate in data modules
    Added `audioKey` field to `AthkarEntity` model and updated all instances in daily, fast, and prayer data modules with corresponding audio keys from the `audio_keys.dart` utility. Included necessary imports for audio key references in each data file.
    
    This change enables audio association for athkar items across the application.

--- lib/core/entities/athkar_entity.dart
+++ lib/core/entities/athkar_entity.dart
@@ -2,10 +2,12 @@ class AthkarEntity {
   final String title;
   final String content;
   final String imageCharacter;
+  final String audioKey;
 
   const AthkarEntity({
     required this.title,
     required this.content,
     required this.imageCharacter,
+    required this.audioKey,
   });
 }
