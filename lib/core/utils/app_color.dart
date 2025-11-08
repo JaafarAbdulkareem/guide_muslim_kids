f487dd605ec61524ff2ab91433c36213a6e8c54e
King <jafaar.abdulkareem@gmail.com>
2025-11-08 19:45:05 +0530

    Add new color constants for card and about us title
    Add cardColor (white) and titleAboutUs (amber) to AppColor class
    to support UI components requiring distinct color styling.

--- lib/core/utils/app_color.dart
+++ lib/core/utils/app_color.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class AppColor {
   AppColor._();
   static const scaffoldBackgroundColor = Color(0xFFFFFFFF);
+  static const Color cardColor = Color(0xFFFFFFFF);
   static const athkarContainTap = Color(0xFFccb992);
   static const audioButton = Color(0xFFFFFFFF);
+  static const Color titleAboutUs = Color(0xFFFF5722);
 }
