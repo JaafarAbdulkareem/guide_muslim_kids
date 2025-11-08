4e669e4c8adbb1de16ba705082b2ffa2ae7b7897
King <jafaar.abdulkareem@gmail.com>
2025-11-08 16:03:02 +0530

    Add About Us route and view
    Update AppRoute to handle new aboutUs route and add corresponding RouteName constant.
    - Added import for AboutUsView in app_route.dart
    - Updated route case in AppRoute.buildRoute method
    - Added RouteName.aboutUs constant in route_name.dart
    
    Files changed:
    lib/core/route/app_route.dart
    lib/core/route/route_name.dart

--- lib/core/route/app_route.dart
+++ lib/core/route/app_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:guide_muslim_kids/core/route/route_name.dart';
+import 'package:guide_muslim_kids/feature/about_us/ui/about_us_view.dart';
 import 'package:guide_muslim_kids/feature/home/ui/home_view.dart';
 
 class AppRoute {
@@ -8,7 +9,9 @@ class AppRoute {
       // case RouteName.splash:
       // return MaterialPageRoute(builder: (_) => const SplashView());
       case RouteName.home:
-      return MaterialPageRoute(builder: (_) => const HomeView());
+        return MaterialPageRoute(builder: (_) => const HomeView());
+      case RouteName.aboutUs:
+        return MaterialPageRoute(builder: (_) => const AboutUsView());
       default:
         return MaterialPageRoute(builder: (_) => const Center());
     }
--- lib/core/route/route_name.dart
+++ lib/core/route/route_name.dart
@@ -2,4 +2,5 @@ class RouteName {
   RouteName._();
   static const String home = '/';
   static const String daily = '/daily';
+  static const String aboutUs = '/aboutUs';
 }
