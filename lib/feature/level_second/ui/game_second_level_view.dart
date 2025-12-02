f2ccad52c4e769b9988bfae9ae00cf005c4e1b25
King <jafaar.abdulkareem@gmail.com>
2025-12-02 14:17:32 +0530

    Create new game second level UI components
    Added GameSecondLevelView and BodyGameSecondLevelView widgets for level 2 game interface.
    GameSecondLevelView provides scaffold with background color and safe area,
    BodyGameSecondLevelView currently contains placeholder for future implementation.
    
    Files created:
    - lib/feature/level_second/ui/game_second_level_view.dart
    - lib/feature/level_second/ui/widget/body_game_second_level_view.dart

---
+++ lib/feature/level_second/ui/game_second_level_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:guide_muslim_kids/core/utils/app_color.dart';
+import 'package:guide_muslim_kids/feature/level_second/ui/widget/body_game_second_level_view.dart';
+
+class GameSecondLevelView extends StatelessWidget {
+  const GameSecondLevelView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.lettersBackground,
+      body: SafeArea(child: BodyGameSecondLevelView()),
+    );
+  }
+}
---
+++ lib/feature/level_second/ui/widget/body_game_second_level_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BodyGameSecondLevelView extends StatelessWidget {
+  const BodyGameSecondLevelView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
