56c82d79f2e72d77b8f299f0539e9e75b7b5ab7a
King <jafaar.abdulkareem@gmail.com>
2025-12-03 14:48:13 +0530

    Rename class to match file location
    Corrected `GameSecondLevelView` to `GameThirdLevelView` in `game_third_level_view.dart` to align with the `level_third` directory structure. This ensures consistency between class names and their feature location in the project hierarchy.

--- lib/feature/level_third/ui/game_third_level_view.dart
+++ lib/feature/level_third/ui/game_third_level_view.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:guide_muslim_kids/core/utils/app_color.dart';
 import 'package:guide_muslim_kids/feature/level_third/ui/widget/body_game_third_level_view.dart';
 
-class GameSecondLevelView extends StatelessWidget {
-  const GameSecondLevelView({super.key});
+class GameThirdLevelView extends StatelessWidget {
+  const GameThirdLevelView({super.key});
 
   @override
   Widget build(BuildContext context) {
